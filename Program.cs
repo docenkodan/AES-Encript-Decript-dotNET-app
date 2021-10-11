a964669b98bbf9f913d8498d82089f7fb319c99c
docenkodan <docenkodan@gmail.com>
2021-10-11 19:12:01 +0300

    program modified

--- Program.cs
+++ Program.cs
@@ -7,156 +7,273 @@ using System.Security.Cryptography;
 
 namespace AES1
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-            Console.Write("Введите шифруемую строку: ");
-            string clearText = Console.ReadLine();
-            Console.Write("Введите секретный ключ: ");
-            string secretKey = Console.ReadLine();
-            Console.Write("Введите вектор инициализации: ");
-            string initVector = Console.ReadLine();
-
-            string command = "";
-            while (command != "0")
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "y";
+            while (command == "y")
             {
-                Console.WriteLine("Шифруемая строка: " + clearText);
-                Console.WriteLine("Секретный ключ: " + secretKey);
-                Console.WriteLine("Вектор инициализации: " + initVector);
-                Console.WriteLine("Для изменения параметров введите:\n" +
-                    "1 - для изменения шифруемой строки\n" +
-                    "2 - для изменения секретного ключа\n" +
-                    "3 - для изменения вектора инициализации\n" +
-                    "Введите 0 для запуска шифрования");
-                command = Console.ReadLine();
+                StartCryption SC = new StartCryption();
 
-                if (command == "1")
+                Console.WriteLine("Выберете режим работы. (e - шифрование, d - дешифрование)");
+                command = Console.ReadLine();
+                if (command == "e")
                 {
-                    Console.Write("Введите шифруемую строку: ");
-                    clearText = Console.ReadLine();
+                    SC.StartEncrypt();
                 }
-                else if (command == "2")
+                else if (command == "d")
                 {
-                    Console.Write("Введите секретный ключ: ");
-                    secretKey = Console.ReadLine();
+                    SC.StartDecrypt();
                 }
-                else if (command == "3")
+
+                Console.WriteLine("Продолжить работу с приложением? (y - да, n - нет)");
+                command = Console.ReadLine();
+            }
+        }
+
+        public sealed class StartCryption
+        {
+            private string data;
+            private string secretKey;
+            private string initVector;
+            private string cipherMode;
+
+            public void ReadParams(string operatingMode)
+            {
+                if (operatingMode == "encrypt")
                 {
-                    Console.Write("Введите вектор инициализации: ");
-                    initVector = Console.ReadLine();
+                    Console.WriteLine("Введите схему шифрования: (ECB, CBC, OFB, CFB, CTS)");
+                    cipherMode = Console.ReadLine();
+                    Console.WriteLine("Введите шифруемую строку: ");
+                    data = Console.ReadLine();
+                }
+                else if (operatingMode == "decrypt")
+                {
+                    Console.WriteLine("Введите схему дешифрования: (ECB, CBC, OFB, CFB, CTS)");
+                    cipherMode = Console.ReadLine();
+                    Console.WriteLine("Введите зашифрованную строку в формате base64: ");
+                    data = Console.ReadLine();
+                }
+                Console.WriteLine("Введите секретный ключ: ");
+                secretKey = Console.ReadLine();
+                Console.WriteLine("Введите вектор инициализации: ");
+                initVector = Console.ReadLine();
+
+                string command = "";
+                while (command != "0")
+                {
+                    if (operatingMode == "encrypt")
+                    {
+                        Console.WriteLine(
+                            "Схема шифрования: " + cipherMode + "\n" +
+                            "Шифруемая строка: " + data + "\n" +
+                            "Секретный ключ: " + secretKey + "\n" +
+                            "Вектор инициализации: " + initVector + "\n" +
+                            "Для изменения параметров введите:\n" +
+                            "1 - для изменения схемы шифрования\n" +
+                            "2 - для изменения шифруемой строки\n" +
+                            "3 - для изменения секретного ключа\n" +
+                            "4 - для изменения вектора инициализации\n" +
+                            "Введите 0 для запуска шифрования"
+                        );
+                    }
+                    else if (operatingMode == "decrypt")
+                    {
+                        Console.WriteLine(
+                            "Схема дешифрования: " + cipherMode + "\n" +
+                            "Зашифрованная строка: " + data + "\n" +
+                            "Секретный ключ: " + secretKey + "\n" +
+                            "Вектор инициализации: " + initVector + "\n" +
+                            "Для изменения параметров введите:\n" +
+                            "1 - для изменения схемы дешифрования\n" +
+                            "2 - для изменения зашифрованной строки\n" +
+                            "3 - для изменения секретного ключа\n" +
+                            "4 - для изменения вектора инициализации\n" +
+                            "Введите 0 для запуска дешифрования"
+                        );
+                    }
+                    command = Console.ReadLine();
+                    if (command == "1")
+                    {
+                        if (operatingMode == "encrypt")
+                        {
+                            Console.Write("Введите схему шифрования: (ECB, CBC, OFB, CFB, CTS)");
+                        }
+                        else if (operatingMode == "decrypt")
+                        {
+                            Console.Write("Введите схему дешифрования: (ECB, CBC, OFB, CFB, CTS)");
+                        }
+                        cipherMode = Console.ReadLine();
+                    }
+                    else if (command == "2")
+                    {
+                        if (operatingMode == "encrypt")
+                        {
+                            Console.Write("Введите шифруемую строку: ");
+                        }
+                        else if (operatingMode == "decrypt")
+                        {
+                            Console.Write("Введите зашифрованную строку в формате base64: ");
+                        }
+                        data = Console.ReadLine();
+                    }
+                    else if (command == "3")
+                    {
+                        Console.Write("Введите секретный ключ: ");
+                        secretKey = Console.ReadLine();
+                    }
+                    else if (command == "4")
+                    {
+                        Console.Write("Введите вектор инициализации: ");
+                        initVector = Console.ReadLine();
+                    }
                 }
             }
 
-            Cryption cript = new Cryption(secretKey, initVector);
+            public void StartEncrypt()
+            {
+                ReadParams("encrypt");
 
-            string data = cript.Encrypt(clearText);
+                Cryption cript = new Cryption(secretKey, initVector);
+                byte[] encrypted_byte = cript.Encrypt(data, cipherMode);
 
-            Console.WriteLine("Зашифрованная строка: " + data);
+                Console.WriteLine("Зашифрованная строка в 16-ом виде: " +
+                    System.BitConverter.ToString(encrypted_byte) + "\n" +
+                    "Зашифрованная строка в base64: " +
+                    Convert.ToBase64String(encrypted_byte));
+            }
+            public void StartDecrypt()
+            {
+                ReadParams("decrypt");
 
-            string decrypted = cript.Decrypt(Console.ReadLine());
-            Console.WriteLine("Decrypted String: " + decrypted);
-            Console.ReadLine();
+                Cryption cript = new Cryption(secretKey, initVector);
+                string decrypted = cript.Decrypt(data, cipherMode);
 
+                Console.WriteLine("Расшифрованная строка: " + decrypted);
+            }
         }
 
-		public sealed class Cryption
-		{
-			private RijndaelManaged Algorithm;              //Security.Cryptography.RijndaelManaged
-			private MemoryStream openStream;                //Управляемая версия Security.Cryptography.Rijndael
-			private ICryptoTransform EncryptorDecryptor;    //interface Security.Cryptography.ICryptoTransform
-			private CryptoStream crStream;                  //Security.Cryptography.CryptoStream связывает потоки с криптографическим преобразованием
-			private StreamWriter strWriter;
-			private StreamReader strReader;
+        public sealed class Cryption
+        {
+            private RijndaelManaged Algorithm; //Security.Cryptography.RijndaelManaged
+            private MemoryStream openStream; //Управляемая версия Security.Cryptography.Rijndael
+            private ICryptoTransform EncryptorDecryptor; //interface Security.Cryptography.ICryptoTransform
+            private CryptoStream crStream; //Security.Cryptography.CryptoStream связывает потоки с криптографическим преобразованием
+            private StreamWriter strWriter;
+            private StreamReader strReader;
 
-			private string key_str;
-			private string iv_str;
+            private string key_str;
+            private string iv_str;
 
-			private byte[] key_byte;
-			private byte[] iv_byte;
+            private byte[] key_byte;
+            private byte[] iv_byte;
 
-			private string pwd_str;
-			private byte[] pwd_byte;
+            private string pwd_str;
+            private byte[] pwd_byte;
 
-			public Cryption(string key_val, string iv_val)
-			{
-				key_byte = new byte[32];
-				iv_byte = new byte[32];
+            public Cryption(string key_val, string iv_val)
+            {
+                ChangeKey(key_val);
+                ChangeIV(iv_val);
+            }
 
-				int i;
-				key_str = key_val;
-				iv_str = iv_val;
+            public void ChangeKey(string key_val)
+            {
+                key_byte = new byte[32];
+                key_str = key_val;
 
-/*              key = ASCIIEncoding.ASCII.GetBytes(m_key);
-                iv = ASCIIEncoding.ASCII.GetBytes(m_iv);*/
+                for (int i = 0; i < key_str.Length; i++)
+                {
+                    key_byte[i] = Convert.ToByte(key_str[i]);
+                }
+            }
+            public void ChangeIV(string iv_val)
+            {
+                iv_byte = new byte[32];
+                iv_str = iv_val;
 
-				for (i = 0; i < key_str.Length; i++)
-				{
-					key_byte[i] = Convert.ToByte(key_str[i]);
-				}
-				for (i = 0; i < iv_str.Length; i++)
-				{
-					iv_byte[i] = Convert.ToByte(iv_str[i]);
-				}
-			}
+                for (int i = 0; i < iv_str.Length; i++)
+                {
+                    iv_byte[i] = Convert.ToByte(iv_str[i]);
+                }
+            }
 
-			public string Encrypt(string s)
-			{
-				Algorithm = new RijndaelManaged();
+            public RijndaelManaged ConstructManager(string cipherMode)
+            {
+                Algorithm = new RijndaelManaged();
 
-				Algorithm.BlockSize = 256;
-				Algorithm.KeySize = 256;
-                //Algorithm.Mode = CipherMode.CBC;
+                Algorithm.BlockSize = 256;
+                Algorithm.KeySize = 256;
+                if (cipherMode == "ECB")
+                {
+                    Algorithm.Mode = CipherMode.ECB;
+                }
+                else if (cipherMode == "CBC")
+                {
+                    Algorithm.Mode = CipherMode.CBC;
+                }
+                else if (cipherMode == "OFB")
+                {
+                    Algorithm.Mode = CipherMode.OFB;
+                }
+                else if (cipherMode == "CFB")
+                {
+                    Algorithm.Mode = CipherMode.CFB;
+                }
+                else if (cipherMode == "CTS")
+                {
+                    Algorithm.Mode = CipherMode.CTS;
+                }
+                return Algorithm;
+            }
+
+            public byte[] Encrypt(string s, string cipherMode)
+            {
+                Algorithm = ConstructManager(cipherMode);
 
                 openStream = new MemoryStream();
 
-				EncryptorDecryptor = Algorithm.CreateEncryptor(key_byte, iv_byte);
+                EncryptorDecryptor = Algorithm.CreateEncryptor(key_byte, iv_byte);
 
-				crStream = new CryptoStream(openStream, EncryptorDecryptor, CryptoStreamMode.Write);
-				strWriter = new StreamWriter(crStream);                 //инициализация экз класса для потока UTF8 и буфера размером по умолччанию
-				strWriter.Write(s);                                     //Запись строки s в поток
+                crStream = new CryptoStream(openStream, EncryptorDecryptor, CryptoStreamMode.Write);
+                strWriter = new StreamWriter(crStream); //инициализация экз класса для потока UTF8 и буфера размером по умолччанию
+                strWriter.Write(s);//Запись строки s в поток
 
-				strWriter.Flush();                                      //очистка всех буферов strWriter для защиты от переполнения
-				crStream.FlushFinalBlock();                             //обновляет состояние нижележащего источника данных с текущим состоянием буфера, затем обнуляет буфер
-				pwd_byte = new byte[openStream.Length];
-                openStream.Position = 0;                                //устанавливаем текущую позицию в потоке
-                openStream.Read(pwd_byte, 0, (int)pwd_byte.Length);     //считываем блок байт из потока в буфер
+                strWriter.Flush();//очистка всех буферов strWriter для защиты от переполнения
+                crStream.FlushFinalBlock();//обновляет состояние нижележащего источника данных с текущим состоянием буфера, затем обнуляет буфер
+                pwd_byte = new byte[openStream.Length];
+                openStream.Position = 0; //устанавливаем текущую позицию в потоке
+                openStream.Read(pwd_byte, 0, (int)pwd_byte.Length);// считываем блок байт из потока в буфер
 
                 pwd_str = new UnicodeEncoding().GetString(pwd_byte);
-
-                Console.WriteLine(System.BitConverter.ToString(pwd_byte));
 
                 strWriter.Close();
                 crStream.Close();
                 openStream.Close();
 
-                return Convert.ToBase64String(pwd_byte);
-			}
+                return pwd_byte;
+            }
 
-			public string Decrypt(string s)
-			{
-				Algorithm = new RijndaelManaged();
-
-				Algorithm.BlockSize = 256;
-				Algorithm.KeySize = 256;
-                //Algorithm.Mode = CipherMode.CBC;
+            public string Decrypt(string s, string cipherMode)
+            {
+                Algorithm = ConstructManager(cipherMode);
 
                 openStream = new MemoryStream(Convert.FromBase64String(s));
 
-				EncryptorDecryptor = Algorithm.CreateDecryptor(key_byte, iv_byte);
+                EncryptorDecryptor = Algorithm.CreateDecryptor(key_byte, iv_byte);
                 openStream.Position = 0;
-				crStream = new CryptoStream(openStream, EncryptorDecryptor, CryptoStreamMode.Read);
-				strReader = new StreamReader(crStream);
+                crStream = new CryptoStream(openStream, EncryptorDecryptor, CryptoStreamMode.Read);
+                strReader = new StreamReader(crStream);
 
                 string result = strReader.ReadToEnd();
 
-                strWriter.Close();
+                strReader.Close();
                 crStream.Close();
                 openStream.Close();
 
                 return result;
-			}
-		}
-	}
+            }
+        }
+    }
 }
-
